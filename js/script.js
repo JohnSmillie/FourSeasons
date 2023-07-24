4cb6739dd7694ad60eb4fc71f9749c30312dc47c
JohnSmillie <jsmillie23@gmail.com>
2023-07-24 16:42:52 -0500

    complete rough draft. needs help maybe

--- js/script.js
+++ js/script.js
@@ -1,25 +1,44 @@
+
+
+
+
 function setAndPlay(season) {
     console.log("function called");
+    console.log("season: " + season);
     
     if (season == ""){
         alert("please enter a season.");
         return;
     }
-    document.getElementById("background").style.backgroundSize="100vh";
+    
     switch(season){
         case "SPRING":
-            document.getElementById("background").style.backgroundImage="url(img/spring.jpg)";
+            console.log("case accessed");
+            document.getElementById("imagery").style.backgroundImage="url(img/spring.jpg)";
+            document.getElementById("imagery").style.backgroundSize="50%";
+            document.getElementById("audio").removeAttribute("class", "d-none");
+            document.getElementById("audio").setAttribute("src", "../audio/spring.mp3");
+
             
             break;
         case "SUMMER":
-            document.getElementById("background").style.backgroundImage="url(img/summer.jpg)";
+            document.getElementById("imagery").style.backgroundImage="url(img/summer.jpg)";
+            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementById("audio").removeAttribute("class", "d-none");
+            document.getElementById("audio").setAttribute("src", "../audio/summer.mp3");
             break;
         case "FALL":
         case "AUTUMN":
-            document.getElementById("background").style.backgroundImage="url(img/fall.jpg)";
+            document.getElementById("imagery").style.backgroundImage="url(img/fall.jpg)";
+            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementById("audio").removeAttribute("class", "d-none");
+            document.getElementById("audio").setAttribute("src", "../audio/fall.mp3");
             break;
         case "WINTER":
-            document.getElementById("background").style.backgroundImage="url(img/winter.jpg)";
+            document.getElementById("imagery").style.backgroundImage="url(img/winter.jpg)";
+            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementById("audio").removeAttribute("class", "d-none");
+            document.getElementById("audio").setAttribute("src", "../audio/winter.mp3");
             break;
         default:
             alert("The season is not recongnized.");
