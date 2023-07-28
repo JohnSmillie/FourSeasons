ffcb58c863072407d533b2bc32a93a9b2a766baf
JohnSmillie <jsmillie23@gmail.com>
2023-07-28 13:24:25 -0500

    Final version 1.0

--- js/script.js
+++ js/script.js
@@ -14,29 +14,34 @@ function setAndPlay(season) {
     switch(season){
         case "SPRING":
             console.log("case accessed");
-            document.getElementById("imagery").style.backgroundImage="url(img/spring.jpg)";
-            document.getElementById("imagery").style.backgroundSize="50%";
+            document.getElementsByTagName("body")[0].style.backgroundImage="url(img/spring.jpg)";
+            document.getElementsByTagName("h1")[0].style.color="black";
+            document.getElementsByTagName("p")[0].style.color="black";
+            document.getElementById("home-link").style.color="black";
             document.getElementById("audio").removeAttribute("class", "d-none");
             document.getElementById("audio").setAttribute("src", "../audio/spring.mp3");
 
             
             break;
         case "SUMMER":
-            document.getElementById("imagery").style.backgroundImage="url(img/summer.jpg)";
-            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementsByTagName("body")[0].style.backgroundImage="url(img/summer.jpg)";
+            document.getElementsByTagName("h1")[0].style.color="white";
+            document.getElementsByTagName("p")[0].style.color="white";
             document.getElementById("audio").removeAttribute("class", "d-none");
             document.getElementById("audio").setAttribute("src", "../audio/summer.mp3");
             break;
         case "FALL":
         case "AUTUMN":
-            document.getElementById("imagery").style.backgroundImage="url(img/fall.jpg)";
-            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementsByTagName("body")[0].style.backgroundImage="url(img/fall.jpg)";
+            document.getElementsByTagName("h1")[0].style.color="white";
+            document.getElementsByTagName("p")[0].style.color="white";
             document.getElementById("audio").removeAttribute("class", "d-none");
             document.getElementById("audio").setAttribute("src", "../audio/fall.mp3");
             break;
         case "WINTER":
-            document.getElementById("imagery").style.backgroundImage="url(img/winter.jpg)";
-            document.getElementById("imagery").style.backgroundSize="90%";
+            document.getElementsByTagName("body")[0].style.backgroundImage="url(img/winter.jpg)";
+            document.getElementsByTagName("h1")[0].style.color="white";
+            document.getElementsByTagName("p")[0].style.color="white";
             document.getElementById("audio").removeAttribute("class", "d-none");
             document.getElementById("audio").setAttribute("src", "../audio/winter.mp3");
             break;
